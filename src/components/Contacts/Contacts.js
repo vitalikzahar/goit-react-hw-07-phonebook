d35292bf93c46599473bb50f841f6cb210627f3e
vitalikzahar <vitalikzahar29@gmail.com>
2023-10-01 19:52:25 +0300

    add

---
+++ src/components/Contacts/Contacts.js
@@ -0,0 +1,38 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { Delete } from './Contacts.styled';
+import { deleteContact } from 'redux/operations';
+import {
+  selectContacts,
+  selectFilter,
+  // selectVisibleContacts,
+} from 'redux/selectors';
+import { useEffect } from 'react';
+import { fetchTasks } from 'redux/operations';
+
+export const Contacts = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTasks());
+  }, [dispatch]);
+
+  const contactsList = useSelector(selectContacts);
+  const filterItems = useSelector(selectFilter);
+  const filterUsers = contactsList.items.filter(contact =>
+    contact.name.toLowerCase().includes(filterItems.filter.toLowerCase())
+  );
+  return (
+    <div>
+      <ul>
+        {filterUsers.map(contact => (
+          <li key={contact.id}>
+            {contact.name}:{contact.number}
+            <Delete onClick={() => dispatch(deleteContact(contact.id))}>
+              Delete
+            </Delete>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+import { Provider } from 'react-redux';
 import './index.css';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter basename="/goit-react-hw-07-phonebook">
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
